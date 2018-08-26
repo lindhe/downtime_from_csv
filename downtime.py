12f28f845e4a9843ff9615d0f0b0ca93e59b8715
Andreas Lindhé <andreas@lindhe.io>
2018-08-26 22:04:30 +0200

    Add --csv flag for output formatting

--- downtime.py
+++ downtime.py
@@ -20,16 +20,20 @@ import argparse
 version = "0.0.1"
 
 ################################     OUTPUT     ################################
-def output(changes, output_file=None):# {{{
+def output_data(changes, output_file=None, csv=False, delimiter=','):# {{{
   """ Prints or writes the list of changes, depending on program arguments. """
   if output_file:
     pass
   for c in changes:
-    print(c['time'] + "," + c['status'])
+    if csv:
+      print(c['time'] + delimiter + c['status'])
+    else:
+      # This should be much prettier
+      print(c['time'] + ": " + c['status'])
 # }}}
 
 #################################     MAIN     #################################
-def main(csv_file, delimiter, indicator, status_col, time_col, begin):# {{{
+def main(csv_file, delimiter, indicator, status_col, time_col, begin, csv_format):# {{{
   """ Reads the file and prints all gaps. """
   with csv_file as f:
     csv_lines = csv.reader(f, delimiter=delimiter)
@@ -43,8 +47,8 @@ def main(csv_file, delimiter, indicator, status_col, time_col, begin):# {{{
       if s != prev:
         changes.append({'time': t, 'status': s})
         prev = s
-  output(changes)# }}}
-
+  output_data(changes, csv=csv, delimiter=delimiter, indicator=indicator)
+# }}}
 
 ############################     BOOTSTRAPPING     ############################
 if __name__ == '__main__':# {{{
@@ -56,6 +60,7 @@ if __name__ == '__main__':# {{{
   # p.add_argument('-o', '--out-file', type=argparse.FileType('w'), help="not implemented")
   # p.add_argument('-u', '--uptime', action='store_true', help="get uptime instead of downtime")
   p.add_argument('-b', '--begin', type=int, default=2, metavar='B', help="number of lines to skip at the beginning (default: 2)")
+  p.add_argument('-c', '--csv', action='store_true', help="output CSV format")
   p.add_argument('-d', '--delimiter', default=',', metavar='D', help="delimiter (default: ,)")
   p.add_argument('-i', '--indicator', default='TRUE', metavar='I', help="the string indicating downtime (default: \"TRUE\")")
   p.add_argument('-s', '--status-column', type=int, default=3, metavar='S', help="index of status column (defualt: 2)")
@@ -72,7 +77,8 @@ if __name__ == '__main__':# {{{
         indicator = args.indicator,
         status_col = args.status_column,
         time_col = args.time_column,
-        begin = args.begin
+        begin = args.begin,
+        csv_format = args.csv,
         )
   except KeyboardInterrupt:
     sys.exit("\nInterrupted by ^C\n")
