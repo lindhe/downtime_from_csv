0f5840e5e3c99d61885c0b9f198805859d3f754d
Andreas Lindhé <andreas@lindhe.io>
2018-08-27 22:07:03 +0200

    Implement output file

--- downtime.py
+++ downtime.py
@@ -20,7 +20,7 @@ import argparse
 version = "0.0.1"
 
 ################################     OUTPUT     ################################
-def output_data(changes, output_file=None, csv=False, delimiter=',', indicator='TRUE'):# {{{
+def output_data(changes, output_file=None, csv_format=False, delimiter=',', indicator='TRUE'):# {{{
   """ Prints or writes the list of changes, depending on program arguments. """
   downtime = []
   if changes[0]['status'] != indicator:
@@ -31,16 +31,28 @@ def output_data(changes, output_file=None, csv=False, delimiter=',', indicator='
   while n < len(changes):
     downtime.append({'from': changes[n]['time'], 'to': changes[n+1]['time']})
     n += 2
-  if csv:
+  if csv_format:
+    if output_file:
+      with output_file as f:
+        for c in changes:
+          csv_line = c['time'] + delimiter + c['status'] + '\n'
+          f.write(csv_line)
     for c in changes:
-      print(c['time'] + delimiter + c['status'])
+      csv_line = c['time'] + delimiter + c['status']
+      print(csv_line)
   else:
+    if output_file:
+      with output_file as f:
+        for span in downtime:
+          human_readable = "Downtime from [" + span['from'] + "] to [" + span['to'] + "]\n"
+          f.write(human_readable)
     for span in downtime:
-      print("Downtime from [" + span['from'] + "] to [" + span['to'] + "]")
+      human_readable = "Downtime from [" + span['from'] + "] to [" + span['to'] + "]"
+      print(human_readable)
 # }}}
 
 #################################     MAIN     #################################
-def main(csv_file, delimiter, indicator, status_col, time_col, begin, csv_format):# {{{
+def main(csv_file, delimiter, indicator, status_col, time_col, begin, csv_format, output):# {{{
   """ Reads the file and prints all gaps. """
   with csv_file as f:
     csv_lines = csv.reader(f, delimiter=delimiter)
@@ -54,7 +66,7 @@ def main(csv_file, delimiter, indicator, status_col, time_col, begin, csv_format
       if s != prev:
         changes.append({'time': t, 'status': s})
         prev = s
-  output_data(changes, csv=csv_format, delimiter=delimiter, indicator=indicator)
+  output_data(changes, csv_format=csv_format, delimiter=delimiter, indicator=indicator, output_file=output)
 # }}}
 
 ############################     BOOTSTRAPPING     ############################
@@ -63,13 +75,12 @@ if __name__ == '__main__':# {{{
   # Arguments
   p = argparse.ArgumentParser(description=desc)
   # p.add_argument('-e', '--end', type=int, default=0, metavar='E', help="number of lines to skip at the bottom (default: 0)")
-  # p.add_argument('-f', '--date-format', help="not implemented")
-  # p.add_argument('-o', '--out-file', type=argparse.FileType('w'), help="not implemented")
   # p.add_argument('-u', '--uptime', action='store_true', help="get uptime instead of downtime")
   p.add_argument('-b', '--begin', type=int, default=2, metavar='B', help="number of lines to skip at the beginning (default: 2)")
   p.add_argument('-c', '--csv', action='store_true', help="output CSV format")
   p.add_argument('-d', '--delimiter', default=',', metavar='D', help="delimiter (default: ,)")
   p.add_argument('-i', '--indicator', default='TRUE', metavar='I', help="the string indicating downtime (default: \"TRUE\")")
+  p.add_argument('-o', '--output', type=argparse.FileType('a'), help="output file")
   p.add_argument('-s', '--status-column', type=int, default=3, metavar='S', help="index of status column (defualt: 2)")
   p.add_argument('-t', '--time-column', type=int, default=2, metavar='T', help="index of timestamps (defualt: 3)")
   p.add_argument('-V', '--version', action='version', version=version)
@@ -86,6 +97,7 @@ if __name__ == '__main__':# {{{
         time_col = args.time_column,
         begin = args.begin,
         csv_format = args.csv,
+        output = args.output
         )
   except KeyboardInterrupt:
     sys.exit("\nInterrupted by ^C\n")
